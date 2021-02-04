dcc59b1facb980d5d3b9118e9401afefbb9b600d
Kiremode <65949758+Kiremode@users.noreply.github.com>
2021-02-04 11:23:39 +0100

    test mal bitte

---
+++ Assets/Scripts/Kuofershmid/puzzle block/CubeActivateDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActivateDoor : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == cube)
+        {
+            Debug.Log("Cube is in it");
+        }
+    }
+}
