9166e405888e7006d15c0ce2d5f842541631cf7c
Kiremode <65949758+Kiremode@users.noreply.github.com>
2021-02-09 14:43:55 +0100

    hjm,

--- Assets/Scripts/Kuofershmid/puzzle block/DamageThePlayer.cs
+++ Assets/Scripts/Kuofershmid/puzzle block/DamageThePlayer.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class DamageThePlayer : MonoBehaviour
 {
+    public int damageThePl;
+    public string tagName;
     // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.tag == tagName)
+        {
+            DamageCalc();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DamageCalc()
     {
-        
+        Helth.health -= damageThePl;
     }
 }
