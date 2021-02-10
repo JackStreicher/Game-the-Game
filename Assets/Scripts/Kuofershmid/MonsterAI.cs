5bfc36df50aeb50871150b43ee6085c9a0cd777b
DomiZone1 <74203314+DomiZone1@users.noreply.github.com>
2021-02-10 12:01:10 +0100

    Bugfixes

--- Assets/Scripts/Kuofershmid/MonsterAI.cs
+++ Assets/Scripts/Kuofershmid/MonsterAI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor.SceneManagement;
+﻿using System;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,35 +8,49 @@ public class MonsterAI : MonoBehaviour
     private GameObject player;
     public NavMeshAgent agent;
     private Collider vision;
+    private bool isInSight;
+    
     public void Start()
     {
         player = GameObject.FindWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
-        vision = GameObject.FindWithTag("VisionCone").
+        vision = GameObject.FindWithTag("VisionCone").gameObject.GetComponent<MeshCollider>();
     }
     
     public void FixedUpdate()
     {
-        IsPlayerVisible();
+        if (IsPlayerVisible())
+        {
+            SelectTarget();
+        }
     }
-
+    
     public void SelectTarget()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 
     public bool IsPlayerVisible()
     {
-
-        
-        if ()
+        if(isInSight)
         {
-            Debug.Log("I can See you\n-The enemy");
+            //Debug.Log("I can See you\n-The enemy");
+            return true;
         }
         else
         {
-            Debug.Log("Cant see player");
+            //Debug.Log("Cant see player");
+            return false;
         }
-        return false;
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Player")) isInSight = true;
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.transform.CompareTag("Player")) isInSight = false;
     }
 }
