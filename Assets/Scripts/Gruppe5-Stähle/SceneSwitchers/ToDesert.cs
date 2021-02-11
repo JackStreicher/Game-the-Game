1915f0e9a7eeb937e65a70a67ab6f6b124aeb1dd
GC-000 <narulio@aol.de>
2021-02-11 11:45:15 +0100

    UnityDEVProjekt

--- Assets/Scripts/Gruppe5-Stähle/SceneSwitchers/ToDesert.cs
+++ Assets/Scripts/Gruppe5-Stähle/SceneSwitchers/ToDesert.cs
@@ -7,6 +7,6 @@ public class ToDesert : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("DesertScene");
     }
 }
--- Assets/Scripts/Gruppe5-Stähle/SceneSwitchers/ToTheCrossing.cs
+++ Assets/Scripts/Gruppe5-Stähle/SceneSwitchers/ToTheCrossing.cs
@@ -7,6 +7,6 @@ public class ToTheCrossing : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("The Crossing");
     }
 }
