42060956a083b83df34f6dadfa3adcb32f88dfcd
Akkiirah <46369555+Akkiira@users.noreply.github.com>
2021-02-11 10:21:58 +0100

    plop

---
+++ Assets/Scripts/Kuofershmid/PortalBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PortalBehaviour : MonoBehaviour
+{
+    public GameObject loadingScreenUi;
+    public BoxCollider triggerPoint;
+    public Scene theCrossingScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Do smth");
+        ChangeScene();
+    }
+
+    private void ChangeScene(int _sceneIndex)
+    {
+        Debug.Log("he might do smth");
+        if (!loadingScreenUi.activeSelf)
+            loadingScreenUi.SetActive(true);
+
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        Debug.Log("he is doing smth");
+        AsyncOperation operation = SceneManager.LoadSceneAsync(//SCENE);
+
+        yield return 0;
+    }
+}
